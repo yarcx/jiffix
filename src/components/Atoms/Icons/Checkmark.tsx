3bba00ea841a29e2ce06453685ce242606a9d108
Hassan Yakubu <55262580+yarcx@users.noreply.github.com>
2022-12-14 15:23:41 +0100

    Build fixes

--- src/components/Atoms/Icons/Checkmark.tsx
+++ src/components/Atoms/Icons/Checkmark.tsx
@@ -1,15 +1,8 @@
 import React from "react";
-import { IconProps } from "src/Interface/Icons";
 
-export function Checkmark({ size, color }: IconProps): JSX.Element {
+export function Checkmark({ size, color }: { size?: number; color?: string }): JSX.Element {
   return (
-    <svg
-      width="11"
-      height="9"
-      viewBox="0 0 11 9"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg width="11" height="9" viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M3.62344 7.10625L1.45469 4.9375C1.21094 4.69375 0.817187 4.69375 0.573437 4.9375C0.329688 5.18125 0.329688 5.575 0.573437 5.81875L3.18594 8.43125C3.42969 8.675 3.82344 8.675 4.06719 8.43125L10.6797 1.81875C10.9234 1.575 10.9234 1.18125 10.6797 0.937497C10.4359 0.693747 10.0422 0.693747 9.79844 0.937497L3.62344 7.10625Z"
         fill="black"
